aa53cfe1e8fa295c2fb90524b383227b6dca5e8a
Lilit Khamalyan <lilitkhamalyan@gmail.com>
2020-01-22 13:10:57 -0500

    Added comments.

--- arithmetic_operators.py
+++ arithmetic_operators.py
@@ -1,11 +1,15 @@
+# Get numbers from the user
 n1 = int(input("Enter the first number: "))
 n2 = int(input("Enter the second number: "))
 
+# Calculate the sum of the two numbers and display
 sumOfNumbers = n1 + n2
 print("The sum of the two numbers is:", sumOfNumbers)
 
+# Calculate the difference of the two numbers and display
 difOfNumbers = n1 - n2
 print("The difference of the two numbers(first - second) is:", difOfNumbers)
 
+# Calculate the product of the two numbers and display
 productOfNumbers = n1 * n2
 print("The product of the two numbers is:", productOfNumbers)
