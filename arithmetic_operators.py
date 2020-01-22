5adf56c6747535d321ef3b6f6052f25e6fc7c37b
Lilit Khamalyan <lilitkhamalyan@gmail.com>
2020-01-22 13:07:48 -0500

    Added difference and product of two numbers.

--- arithmetic_operators.py
+++ arithmetic_operators.py
@@ -1,5 +1,11 @@
 n1 = int(input("Enter the first number: "))
 n2 = int(input("Enter the second number: "))
 
-sumOfNumbers = n1 +n2
+sumOfNumbers = n1 + n2
 print("The sum of the two numbers is:", sumOfNumbers)
+
+difOfNumbers = n1 - n2
+print("The difference of the two numbers(first - second) is:", difOfNumbers)
+
+productOfNumbers = n1 * n2
+print("The product of the two numbers is:", productOfNumbers)
